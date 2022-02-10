20fced27eb7450164309faf9170540168ef11481
pritam1813 <46365255+pritam1813@users.noreply.github.com>
2022-02-10 11:19:36 +0530

    Update TreeNode.h

--- DSA/Lecture 11 - Trees/TreeNode.h
+++ DSA/Lecture 11 - Trees/TreeNode.h
@@ -1,1 +1,24 @@
+#include <vector>
 
+using namespace std;
+template <typename T>
+
+class TreeNode{
+    public:
+
+    T data;
+    vector<TreeNode<T>*> children;
+	
+	//Constructor
+    TreeNode(T data){
+        this -> data = data;
+    }
+
+	// Destructor
+    ~TreeNode (){
+        for(int i = 0; i < children.size(); i++){
+            delete children[i];
+        }
+    }
+
+};
