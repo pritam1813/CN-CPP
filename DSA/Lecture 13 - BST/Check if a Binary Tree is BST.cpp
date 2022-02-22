b45b9edcc5d13c76863430523db133b2bf8db0e5
pritam1813 <46365255+pritam1813@users.noreply.github.com>
2022-02-22 13:21:28 +0530

    Update Check if a Binary Tree is BST.cpp

--- DSA/Lecture 13 - BST/Check if a Binary Tree is BST.cpp
+++ DSA/Lecture 13 - BST/Check if a Binary Tree is BST.cpp
@@ -5,60 +5,87 @@
 
 using namespace std;
 
- /*
+/*
+// Approach 1 O(n * h)
+
+int minimum(BinaryTreeNode<int> *root){
+    if(root == NULL){
+        return INT_MAX;
+    }
+    return min(root -> data,min(minimum(root -> left),minimum(root -> right)));
+}
+
+int maximum(BinaryTreeNode<int> *root){
+    if(root == NULL){
+        return INT_MIN;
+    }
+    return max(root -> data,max(maximum(root -> left),maximum(root -> right)));
+}
 bool isBST(BinaryTreeNode<int> *root) {
-  // MY APPROACH O(N * H)
-  if(root == NULL){
+    if(root == NULL){
         return true;
     }
     
-    if(root -> left == NULL && root -> right == NULL){
-        return true;
-    }
-    
-    if(root -> left != NULL && root -> left -> data >= root -> data){
-        return false;
-    }
-    
-    if(root -> right != NULL && root -> right -> data < root -> data){
-        return false;
-    }
-    
-    bool leftAns = true;
-    
-    if(root -> left != NULL){
-        leftAns = isBST(root -> left);
-    }
-    
-    bool rightAns = true;
-    
-    if(root -> right != NULL){
-        rightAns = isBST(root -> right);
-    }
-    
-    return leftAns && rightAns;
- }
-    */
-// More Efficient Approach
+    int leftMax = maximum(root -> left);
+    int rightMin = minimum(root -> right);
 
-bool isBSThelper(BinaryTreeNode<int> *root, int min = INT_MIN, int max = INT_MAX)
-{
-    if (root == NULL)
-    {
-        return true;
-    }
+    return (root -> data > leftMax) && (root -> data <= rightMin) && isBST(root -> left) && isBST(root -> right); 
+}
+*/
 
-    if (root->data < min || root->data > max)
-    {
-        return false;
-    }
+/*
+// Approach 2 O(n)
 
-    return isBSThelper(root->left, min, root->data - 1) &&
-           isBSThelper(root->right, root->data, max);
+pair<pair<int,int>,bool> helper(BinaryTreeNode<int> *root){
+    if(root == NULL){
+        pair<pair<int,int>,bool> p1;
+        pair<int,int> p2;
+        p2.first = INT_MAX;  //This member is denoting Minimum value
+        p2.second = INT_MIN;  ////This member is denoting Maximum value
+        p1.first = p2;
+        p1.second = true;
+        return p1;
+    }
+    pair<pair<int,int>,bool> leftAns = helper(root -> left);
+    pair<pair<int,int>,bool> rightAns = helper(root -> right);
+
+    pair<pair<int,int>,bool> ans;
+    ans.first.first = min(root -> data,min(leftAns.first.first, rightAns.first.first));          //Overall minimum
+    ans.first.second = max(root -> data,max(leftAns.first.second, rightAns.first.second));       //Overall maximum
+
+          // overall isBST by comparing rootData with leftMax and rightMin and also checking left isBST && right isBST
+          
+    ans.second = (root -> data > leftAns.first.second) && (root -> data <= rightAns.first.first) && leftAns.second && rightAns.second;
+    return ans;
 }
 
 bool isBST(BinaryTreeNode<int> *root) {
-    return isBSThelper(root, INT_MIN, INT_MAX);
+    pair<pair<int,int>,bool> Ans = helper(root);
+    return Ans.second;
+}
+*/
+
+
+// Approach 3 pushing down the constraints O(n)
+
+bool helper(BinaryTreeNode<int> *root, int min, int max){
+    if(root == NULL){
+        return true;
+    }
+    // Considering rootData is between -infinity to +infinity for first time
+ 
+    if(root -> data < min || root -> data > max){
+        return false;
+    }
+ 
+    // Changing cnstraint while calling left root->Left Data will be greater than -infinity but less than root Data
+    // Similarly for calling right , root -> right data is greater than equal to root data but less than +infinity  
+ 
+    return helper(root -> left,min,root -> data - 1) && helper(root -> right,root -> data,max);
+}
+
+bool isBST(BinaryTreeNode<int> *root) {
+    return helper(root, INT_MIN, INT_MAX);
 }
 
 int main() {
