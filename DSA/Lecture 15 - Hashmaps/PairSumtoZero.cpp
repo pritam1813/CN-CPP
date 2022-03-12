33021523e38c624c430f84ed4f3ad2ecccc67ac4
pritam1813 <46365255+pritam1813@users.noreply.github.com>
2022-03-12 12:07:08 +0530

    Add files via upload

---
+++ DSA/Lecture 15 - Hashmaps/PairSumtoZero.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int pairSum(int *arr, int n) {
+    // Write your code here
+    unordered_map<int,int> myMap;
+    int count = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        myMap[arr[i]]++;
+        
+        if(myMap.count(-arr[i]) != 0){
+            count += myMap[-arr[i]];
+            if(arr[i] == -arr[i]){
+                count--;
+            }
+        }
+    }
+    return count;
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    int* arr = new int[n];
+
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    cout << pairSum(arr, n);
+
+    delete[] arr;
+}
